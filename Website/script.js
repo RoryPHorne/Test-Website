99e8bae48044176e5e4f8c79d668bfbf089cdd7a
Rory Horne <roryphorne@gmail.com>
2026-07-22 12:30:20 +0100

    Add files via upload

---
+++ Website/script.js
@@ -0,0 +1,8 @@
+//External JavaScript Example
+function changeText() {
+    var heading = document.getElementById("demo")
+    heading.textContent = "New Text!";
+}
+window.onload = function () {
+    alert("This is a website to test JavaScript code in");
+}
