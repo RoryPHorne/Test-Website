323db402b64722adab67001f452052e379846bcc
Rory Horne <roryphorne@gmail.com>
2026-07-22 12:31:42 +0100

    Delete Website directory

--- Website/script.js
+++
@@ -1,8 +0,0 @@
-//External JavaScript Example
-function changeText() {
-    var heading = document.getElementById("demo")
-    heading.textContent = "New Text!";
-}
-window.onload = function () {
-    alert("This is a website to test JavaScript code in");
-}
